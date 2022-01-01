a3d3deca0618ecb8b21c7bdbef4aa652cb82c48c
lenski88 <lenski88@icloud.com>
2022-01-01 06:48:57 +0300

    feat: add Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
+import { Navbar } from "./components/Navbar";
 import { BookContextProvider } from "./context/BookContext";
-
+import { GlobalStyles } from "./globalStyles";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-     <BookContextProvider></BookContextProvider>
-    </div>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <BookContextProvider>
+          <Navbar />
+        </BookContextProvider>
+      </StyledApp>
+    </>
   );
 }
 
 export default App;
+
+const StyledApp = styled.div`
+  min-height: 90vh;
+  max-width: 960px;
+  background-color: #333745;
+  margin: 30px auto;
+  border-radius: 10px;
+  box-shadow: #70768d 0px 0px 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  color: #fff;
+  overflow: auto;
+`;
---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+import { BookContext } from "../context/BookContext";
+import styled from 'styled-components';
+
+export const Navbar = () => {
+  const books = useContext(BookContext);
+  return (
+    <StyledNavbar>
+      <h1>Reading list</h1>
+      <p>Currently you have {books.length} books</p>
+    </StyledNavbar>
+  );
+};
+
+const StyledNavbar = styled.div`
+    text-align: center;
+    h1 {
+        font-size: 2.4rem;
+        margin:10px
+    }
+    p {
+        font-size: 1.6rem;
+    }
+`
